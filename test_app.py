d450295d2260e83f08b9f191db0bba9a3510b143
m-abduulah <i210273@nu.edu.pk>
2024-09-15 17:10:49 +0500

    Model Training

--- test_app.py
+++ test_app.py
@@ -7,7 +7,11 @@ def client():
     with app.test_client() as client:
         yield client
 
-
+def test_home_page(client):
+    """Test the home page"""
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Welcome to the Home Page" in response.data
 
 def test_predict_endpoint(client):
     """Test the prediction endpoint"""
